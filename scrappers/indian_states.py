fb641a985b521a6779a4cb7d27e1868b885737e2
Ariyan S <developer.ariyanas@gmail.com>
2018-11-29 00:09:28 +0530

    states scrapper working

--- scrappers/indian_states.py
+++ scrappers/indian_states.py
@@ -2,44 +2,31 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-URL = "https://en.wikipedia.org/wiki/States_and_union_territories_of_India"
+URL = "https://simple.wikipedia.org/wiki/States_and_territories_of_India"
 
 r = requests.get(URL)
 
 soup = BeautifulSoup(r.content, 'html5lib')
 
-table = soup.findAll('table', attrs={'class': 'wikitable'})
-# tbody = table.tbody
-
-# print(table)
-i = 0
-for tbl in table:
-    print(i)
-    print(tbl)
-    i = i + 1
+tables = soup.findAll('table', attrs={'class': 'wikitable'})
 
 states = []
 
-# for tr in tbody.findAll('tr'):
-#     state = {}
-#     th = tr.find('th')
-#     tds = tr.findAll('td')
+tbody_state = tables[0].find('tbody')
 
-#     state['name'] = th
-#     # state['capital'] = tds[2]
+for tr in tbody_state.findAll('tr')[1:]:
+    tds = tr.findAll('td')
+    state = {}
 
-#     # print(tds)
+    state['name'] = tds[0].a.text
+    state['capital'] = tds[1].text
+    state['code'] = tds[2].text[0:2]
 
-#     states.append(state)
+    states.append(state)
 
-# print(states)
+states_file = '../results/indian_states.csv'
 
-# states_file = '../results/indian_states.csv'
-
-#print(countries)
-
-# with open(country_file, 'w', newline='') as f:
-#     wr = csv.DictWriter(f, ['name', 'isd', 'code', 'code_full'])
-#     wr.writeheader()
-#     for c in countries:
-#         wr.writerow(c)
+with open(states_file, 'w', newline='') as f:
+    wr = csv.DictWriter(f, ['name', 'capital', 'code'])
+    wr.writeheader()
+    wr.writerows(states)
